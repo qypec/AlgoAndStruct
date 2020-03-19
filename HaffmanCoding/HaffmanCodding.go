7746c26d25a50df6614fac60a4a4ffd5751826b0
qypec <qypec.prog@yandex.ru>
2020-03-19 13:33:37 +0300

    HaffmanCodding v1

--- HaffmanCoding/HaffmanCodding.go
+++ HaffmanCoding/HaffmanCodding.go
@@ -1,188 +1,114 @@
 package main
 
 import (
-	"github.com/AlgoAndStruct/myContainers/heap"
 	"bufio"
 	"fmt"
+	"github.com/AlgoAndStruct/myContainers/binaryTree"
+	"github.com/AlgoAndStruct/myContainers/heap"
 	"os"
-	"strconv"
 )
 
-type myInt struct {
-	x int
+type letter struct {
+	frequency int
+	ch rune
 }
 
-func (i myInt) Priority() int {
-	return i.x
+type EncryptedInfo struct {
+	frequencyTable map[rune]int
+	letterTable map[rune][]rune
+	str []rune
+}
+
+func ComposeFrequencyTable(unencrypted string) map[rune]int {
+	frequencyTable := make(map[rune]int)
+	for _, ch := range unencrypted {
+		frequencyTable[rune(ch)]++
+	}
+	return frequencyTable
+}
+
+func MakeParent(leftChild, rightChild *binaryTree.Element) *binaryTree.Element {
+	parent := binaryTree.MakeParent(leftChild, rightChild)
+	parent.Value = letter{
+		leftChild.Value.(letter).frequency + rightChild.Value.(letter).frequency,
+		'0',
+	}
+	return parent
+}
+
+func isLetterElement(elem *binaryTree.Element) bool {
+	return elem.Value.(letter).ch != '0'
+}
+
+func FillLetterTable(elem *binaryTree.Element, code []rune, letterTable map[rune][]rune) []rune {
+	if isLetterElement(elem) {
+		letterTable[elem.Value.(letter).ch] = append(letterTable[elem.Value.(letter).ch], code...)
+		if len(code) > 0 {
+			code = code[:len(code)-1]
+		}
+	}
+	if elem.ChildLeft != nil {
+		code = append(code, rune('0'))
+		code = FillLetterTable(elem.ChildLeft, code, letterTable)
+	}
+	if elem.ChildRight != nil {
+		code = append(code, rune('1'))
+		code = FillLetterTable(elem.ChildRight, code, letterTable)
+	}
+	return code
+}
+
+func ComposeEncryptedInfo(encryp *EncryptedInfo, HaffmanTree *binaryTree.Tree, unencrypted string) *EncryptedInfo {
+	var code []rune
+
+	if !HaffmanTree.Root.HasChilds() {
+		code = append(code, rune('0'))
+	}
+	encryp.letterTable = make(map[rune][]rune)
+	FillLetterTable(HaffmanTree.Root, code, encryp.letterTable)
+	for _, letter := range unencrypted {
+		encryp.str = append(encryp.str, encryp.letterTable[rune(letter)]...)
+	}
+	return encryp
+}
+
+func HaffmanAlgorithm(unencrypted string) *EncryptedInfo {
+	var encryp EncryptedInfo
+	var leftChild, rightChild *binaryTree.Element
+	var freqPrQueue heap.MinHeap
+
+	freqPrQueue.Init()
+	encryp.frequencyTable = ComposeFrequencyTable(unencrypted)
+	for ch, frequency := range encryp.frequencyTable { // make a priority queue of tree`s elements
+		freqPrQueue.Insert(frequency, binaryTree.MakeElement(letter{frequency, ch}))
+	}
+	if freqPrQueue.Size() == 0 {
+		return nil
+	}
+
+	for freqPrQueue.Size() != 1 { // make a Haffman tree
+		if ok := freqPrQueue.ExtractMin(); ok != nil { leftChild = ok.(*binaryTree.Element) }
+		if ok := freqPrQueue.ExtractMin(); ok != nil { rightChild = ok.(*binaryTree.Element) }
+		parent := MakeParent(leftChild, rightChild)
+		freqPrQueue.Insert(parent.Value.(letter).frequency, parent)
+	}
+	HaffmanTree := binaryTree.MakeRoot(freqPrQueue.ExtractMin().(*binaryTree.Element))
+	return ComposeEncryptedInfo(&encryp, HaffmanTree, unencrypted)
 }
 
 func main() {
-	var pQueue heap.MinHeap
-
-	pQueue.Init()
-
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
 	scanner.Scan()
 
-	pQueue.Init()
-	result := make([]int, 0)
-	for scanner.Scan() {
-		switch scanner.Text() {
-		case "Insert":
-			scanner.Scan()
-			x, _ := strconv.Atoi(scanner.Text())
-			pQueue.Insert(myInt{x})
-		case "ExtractMin":
-			result = append(result, pQueue.ExtractMin().Priority())
-		}
+	encryptedInfo := HaffmanAlgorithm(scanner.Text())
+	if encryptedInfo == nil {
+		return
 	}
 
-	for _, elem := range(result) {
-		fmt.Println(elem)
+	fmt.Println(len(encryptedInfo.frequencyTable), len(encryptedInfo.str))
+	for k, v := range encryptedInfo.letterTable {
+		fmt.Printf("%c: %s\n", k, string(v))
 	}
+	fmt.Println(string(encryptedInfo.str))
+
 }
-
-/* Tree */
-
-//type Tree struct {
-//	root *TreeElement
-//}
-//
-//type TreeElement struct {
-//	parent *TreeElement
-//	childLeft *TreeElement
-//	childRight *TreeElement
-//
-//	value interface{}
-//}
-//
-//func (t *TreeElement) print() string {
-//	if t == nil {
-//		return ""
-//	}
-//	return fmt.Sprintf("\tparent : %v\n\tchildLeft : %v\n\tchildRight : %v\n \t'%c' %v\n", t.parent, t.childLeft, t.childRight, rune(t.value.(TreeValue).ch), t.value.(TreeValue).frequency)
-//}
-//
-//func (t *TreeElement) Priority() int {
-//	return t.value.(TreeValue).Priority()
-//}
-//
-//func (t *TreeElement) MakeParent(child_1 *TreeElement, child_2 *TreeElement) {
-//	child_1.parent = t
-//	child_2.parent = t
-//	t.childLeft = &TreeElement{child_1.parent, child_1.childLeft, child_1.childRight, child_1.value}
-//	t.childRight = &TreeElement{child_2.parent, child_2.childLeft, child_2.childRight, child_2.value}
-//}
-//
-//
-//type TreeValue struct {
-//	frequency int
-//	ch rune
-//}
-//
-//func (t TreeValue) Priority() int {
-//	return t.frequency
-//}
-//
-//func (t TreeValue) isSymb() bool {
-//	if t.ch != '0' {
-//		return true
-//	}
-//	return false
-//}
-//
-//func NewTreeElement(value interface{}) *TreeElement {
-//	return &TreeElement{nil, nil, nil, value}
-//}
-//
-///* Haffman */
-//
-//type Haffman struct {
-//	codeStringSize int
-//	codeTable map[rune][]rune
-//	code []rune
-//}
-//
-//func FillCodeTable(elem *TreeElement, code []rune, codeTable map[rune][]rune) []rune {
-//	if elem.value.(TreeValue).isSymb() {
-//		codeTable[elem.value.(TreeValue).ch] = append(codeTable[elem.value.(TreeValue).ch], code...)
-//		code = code[:len(code) - 1]
-//	}
-//	if elem.childLeft != nil {
-//		code = append(code, rune('0'))
-//		code = FillCodeTable(elem.childLeft, code, codeTable)
-//	}
-//	if elem.childRight != nil {
-//		code = append(code, rune('1'))
-//		code = FillCodeTable(elem.childRight, code, codeTable)
-//	}
-//	return code
-//}
-//
-//func HaffmanAlgorithm(str string, frequencyTable map[rune]int) *Haffman {
-//	var HaffmanTree Tree
-//	var freqPrQueue MinHeap
-//	var code []rune
-//	var H Haffman
-//	var child_1 TreeElement
-//
-//	freqPrQueue.Init()
-//	for ch, frequency := range(frequencyTable) {
-//		newElem := NewTreeElement(TreeValue{frequency, ch})
-//		freqPrQueue.Insert(newElem.Priority(), *newElem)
-//
-//	}
-//	if freqPrQueue.Size() == 0 {
-//		return nil
-//	}
-//	for i := 1; i < freqPrQueue.Size(); i++ {
-//		bla := freqPrQueue.arr[i].value.(TreeElement)
-//		fmt.Println(bla.print())
-//	}
-//	fmt.Println()
-//
-//	for child_1 = freqPrQueue.ExtractMin().value.(TreeElement); freqPrQueue.Size() != 0; child_1 = freqPrQueue.ExtractMin().value.(TreeElement) {
-//		child_2 := freqPrQueue.ExtractMin().value.(TreeElement)
-//		parent := NewTreeElement(TreeValue{child_1.Priority() + child_2.Priority(), '0'})
-//		fmt.Println(child_1.print())
-//		fmt.Println(child_2.print())
-//		fmt.Println(parent.print())
-//		parent.MakeParent(&child_1, &child_2)
-//		freqPrQueue.Insert(parent.Priority(), *parent)
-//	}
-//	HaffmanTree.root = &child_1
-//
-//	// H.codeStringSize = HaffmanTree.root.Priority()
-//	H.codeTable = make(map[rune][]rune)
-//	FillCodeTable(HaffmanTree.root, code, H.codeTable)
-//
-//	for _, letter := range str {
-//		H.code = append(H.code, H.codeTable[rune(letter)]...)
-//	}
-//	return &H
-//}
-//
-//func main() {
-//	// scanner := bufio.NewScanner(os.Stdin)
-//	// scanner.Scan()
-//
-//	str := "beep boop"
-//
-//	frequencyTable := make(map[rune]int)
-//	for _, ch := range(str/*scanner.Text()*/) {
-//		frequencyTable[rune(ch)]++
-//	}
-//
-//	H := HaffmanAlgorithm(str/*scanner.Text()*/, frequencyTable)
-//	if H == nil {
-//		return
-//	}
-//	H.codeStringSize = len(H.code)
-//	fmt.Println(len(frequencyTable), H.codeStringSize)
-//	for k, v := range H.codeTable {
-//		fmt.Printf("%c: %s\n", k, string(v))
-//	}
-//	fmt.Println(string(H.code))
-//
-//}
