6e081c351b4e4502715b8720a333e886d9aaf7a1
qypec <qypec.prog@yandex.ru>
2020-03-27 17:48:05 +0300

    added upperBound and lowerBound search functions

---
+++ myContainers/sort/lowerBound.go
@@ -0,0 +1,33 @@
+package sort
+
+func isAscending(a []float64) bool {
+	if a[0] <= a[len(a) - 1] {
+		return true
+	}
+	return false
+}
+
+/* finds the first element that is strictly less than the given(toFind) */
+/* array a must be sorted */
+/* log(n) */
+func LowerBound(a []float64, toFind float64, l, r int) int {
+	if isAscending(a) {
+		if a[0] < toFind {
+			return 0
+		} else {
+			return -1
+		}
+	}
+
+	fix := -1
+	for l <= r {
+		m := l + int((r-l)/2)
+		if a[m] < toFind {
+			fix = m
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	return fix
+}
---
+++ myContainers/sort/upperBound_test.go
@@ -0,0 +1,54 @@
+package sort
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type testUppedBound struct {
+	a []float64
+	toFind float64
+}
+
+func TestUpperBound(t *testing.T) {
+	numberOfCases := 0
+
+	testsValue := []testUppedBound{
+		/* 0 */ {[]float64{1, 3, 5, 7, 9, 11, 13, 15}, 3}, // ascending order
+		/* 1 */ {[]float64{15, 13, 11, 9, 7, 5, 3, 1}, 3}, // descending order
+		/* 2 */ {[]float64{1, 3, 5, 7, 9, 11, 13, 15}, 6},
+		/* 3 */ {[]float64{15, 13, 11, 9, 7, 5, 3, 1}, 6},
+		/* 4 */ {[]float64{1, 3, 5, 7, 9, 11, 13, 15}, 20}, // greater than anything ascending
+		/* 5 */ {[]float64{15, 13, 11, 9, 7, 5, 3, 1}, 20}, // greater than anything descending
+		/* 6 */ {[]float64{1, 3, 5, 7, 9, 11, 13, 15}, 14},
+		/* 7 */ {[]float64{15, 13, 11, 9, 7, 5, 3, 1}, 14},
+		/* 8 */ {[]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 14},
+		/* 9 */ {[]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1},
+		/* 10 */ {[]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 0},
+	}
+
+	testsExpected := []int{
+		/* 0 */ 2,
+		/* 1 */ 0,
+		/* 2 */ 3,
+		/* 3 */ 0,
+		/* 4 */ -1,
+		/* 5 */ -1,
+		/* 6 */ 7,
+		/* 7 */ 0,
+		/* 8 */ -1,
+		/* 9 */ -1,
+		/* 10 */ 0,
+	}
+
+	for i := 0; i < len(testsValue); i++ {
+		testCase := testsValue[i]
+		actual := UpperBound(testCase.a, testCase.toFind, 0, len(testCase.a) - 1)
+		require.Equal(t, testsExpected[i], actual, fmt.Sprintf("case %v", i))
+		numberOfCases++
+	}
+
+	fmt.Println("UpperBound. Number of cases: ", numberOfCases)
+
+}
