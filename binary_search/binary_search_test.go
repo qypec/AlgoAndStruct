177f93c218045ce5a0973d5a5a609f086a7118d7
qypec <qypec.prog@yandex.ru>
2020-05-19 15:34:03 +0300

    binary_search | added tests for l, r

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -9,25 +9,31 @@ import (
 type testBinarySearch struct {
 	a      []int
 	toFind int
+	l, r   int
 }
 
 func TestBinarySearch(t *testing.T) {
 	numberOfCases := 0
 
 	testsValue := []testBinarySearch{
-		/* 0 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 3}, // ascending order
-		/* 1 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 3}, // descending order
-		/* 2 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 1},
-		/* 3 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 15},
-		/* 4 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 20}, // greater than anything ascending
-		/* 5 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 20}, // greater than anything descending
-		/* 6 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 15},
-		/* 7 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 1},
-		/* 8 */ {[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1},
-		/* 9 */ {[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 20},
-		/* 10 */ {[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 0},
-		/* 11 */ {[]int{1, 3, 5, 7, 9, 13, 13, 15}, 13},
-		/* 12 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 3},
+		/* 0 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 3, 0, 7}, // ascending order
+		/* 1 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 3, 0, 7}, // descending order
+		/* 2 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 1, 0, 7},
+		/* 3 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 15, 0, 7},
+		/* 4 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 20, 0, 7}, // greater than anything ascending
+		/* 5 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 20, 0, 7}, // greater than anything descending
+		/* 6 */ {[]int{1, 3, 5, 7, 9, 11, 13, 15}, 15, 0, 7},
+		/* 7 */ {[]int{15, 13, 11, 9, 7, 5, 3, 1}, 1, 0, 7},
+		/* 8 */ {[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1, 0, 9},
+		/* 9 */ {[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 20, 0, 9},
+		/* 10 */ {[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 0, 0, 9},
+		/* 11 */ {[]int{1, 3, 5, 7, 9, 13, 13, 15}, 13, 0, 7},
+		/* 12 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 3, 0, 9},
+		/* 13 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 3, 4, 9},
+		/* 14 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 3, 3, 9},
+		/* 15 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 3, 2, 9},
+		/* 16 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 13, 2, 7},
+		/* 17 */ {[]int{1, 3, 3, 3, 5, 7, 9, 13, 13, 15}, 3, 2, 2},
 	}
 
 	testsExpected := []int{
@@ -44,11 +50,16 @@ func TestBinarySearch(t *testing.T) {
 		/* 10 */ -1,
 		/* 11 */ 5,
 		/* 12 */ 1,
+		/* 13 */ -1,
+		/* 14 */ 3,
+		/* 15 */ 2,
+		/* 16 */ 7,
+		/* 17 */ 2,
 	}
 
 	for i := 0; i < len(testsValue); i++ {
 		testCase := testsValue[i]
-		actual := BinarySearch(testCase.a, testCase.toFind, 0, len(testCase.a)-1)
+		actual := BinarySearch(testCase.a, testCase.toFind, testCase.l, testCase.r)
 		require.Equal(t, testsExpected[i], actual, fmt.Sprintf("case %v", i))
 		numberOfCases++
 	}
